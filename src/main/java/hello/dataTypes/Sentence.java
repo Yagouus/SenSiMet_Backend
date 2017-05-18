6223227f5fcda3519b1caf617ca74e4aa4634c1e
Yagouus <yagofontenla95@gmail.com>
2017-05-18 22:08:50 +0200

    CoreNLP and Bfy configured

--- src/main/java/hello/dataTypes/Sentence.java
+++ src/main/java/hello/dataTypes/Sentence.java
@@ -1,4 +1,15 @@
 package hello.dataTypes;
+
+
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.util.CoreMap;
 import it.uniroma1.lcl.babelfy.core.Babelfy;
 import it.uniroma1.lcl.babelfy.commons.annotation.SemanticAnnotation;
 import it.uniroma1.lcl.jlt.util.Language;
@@ -12,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 
 public class Sentence {
@@ -20,16 +32,22 @@ public class Sentence {
     private ArrayList<Term> terms = new ArrayList<>();
 
     public Sentence(String string) {
+
+        //Set the sentence string
         setString(string);
-        //Tokenize();
-        babel();
+
+        //Tokenize and POS
+        Tokenize();
+
+        //Disambiguate terms
+        Disambiguate();
     }
 
-    //Tokenize
+    //Tokenize and dependencies
     public void Tokenize() {
 
         //Initialize OpenNLP tokenizer
-        Tokenizer tokenizer = null;
+        /*Tokenizer tokenizer = null;
         try {
             InputStream is = new FileInputStream("lib/en-token.bin");
             TokenizerModel model = new TokenizerModel(is);
@@ -46,14 +64,69 @@ public class Sentence {
         for (String word : words) {
             this.terms.add(new Term(word));
             System.out.println(word);
+        }*/
+
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
+        Properties props = new Properties();
+        props.put("pos.model", "edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger");
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        System.out.println(string);
+
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(this.string);
+        System.out.println(document);
+
+        // run all Annotators on this text
+        pipeline.annotate(document);
+
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+
+        for(CoreMap sentence: sentences) {
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                // this is the text of the token
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                // this is the POS tag of the token
+                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                // this is the NER label of the token
+                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+            }
+
+            // this is the parse tree of the current sentence
+            Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+
+            // this is the Stanford dependency graph of the current sentence
+            SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
         }
+
     }
 
-    public void babel() {
+    //Disambiguate
+    public void Disambiguate() {
+
+        //Babelfy instance
         Babelfy bfy = new Babelfy();
-        String inputText = "BabelNet is both a multilingual encyclopedic dictionary and a semantic network";
+
         System.out.println(string);
-        List<SemanticAnnotation> bfyAnnotations = bfy.babelfy(inputText, Language.EN);
+
+        //Call bfy API
+        ArrayList<SemanticAnnotation> bfyAnnotations = (ArrayList<SemanticAnnotation>) bfy.babelfy(string, Language.EN);
+
+        //Iterate over list
+        //bfyAnnotations is the result of Babelfy.babelfy() call
+        for (SemanticAnnotation annotation : bfyAnnotations) {
+            //splitting the input text using the CharOffsetFragment start and end anchors
+            String frag = string.substring(annotation.getCharOffsetFragment().getStart(),
+                    annotation.getCharOffsetFragment().getEnd() + 1);
+            System.out.println(frag + "\t" + annotation.getBabelSynsetID());
+            System.out.println("\t" + annotation.getBabelNetURL());
+            System.out.println("\t" + annotation.getDBpediaURL());
+            System.out.println("\t" + annotation.getSource());
+        }
+
         System.out.println(bfyAnnotations);
     }
 
